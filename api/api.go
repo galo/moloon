dacee3bbde7f85350c8359def5ada893783087e3
Galo Gimenez <galo@hp.com>
2019-11-05 08:42:50 -0700

    api

---
+++ api/api.go
@@ -0,0 +1,67 @@
+// Package api configures an http server for administration and application resources.
+package api
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/galo/moloon/api/app"
+	"github.com/galo/moloon/logging"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
+	"github.com/go-chi/render"
+)
+
+// New configures application resources and routes.
+func New() (*chi.Mux, error) {
+	logger := logging.NewLogger()
+
+	appAPI, err := app.NewAPI()
+	if err != nil {
+		logger.WithField("module", "app").Error(err)
+		return nil, err
+	}
+
+	r := chi.NewRouter()
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
+	// r.Use(middleware.RealIP)
+	r.Use(middleware.DefaultCompress)
+	r.Use(middleware.Timeout(15 * time.Second))
+
+	r.Use(logging.NewStructuredLogger(logger))
+	r.Use(render.SetContentType(render.ContentTypeJSON))
+
+	// use CORS middleware if client is not served by this api, e.g. from other domain or CDN
+	// r.Use(corsConfig().Handler)
+
+	r.Group(func(r chi.Router) {
+		r.Mount("/api", appAPI.Router())
+	})
+
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	return r, nil
+}
+
+func corsConfig() *cors.Cors {
+	// Basic CORS
+	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
+	return cors.New(cors.Options{
+		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           86400, // Maximum value not ignored by any of major browsers
+	})
+}
+
+func MakeTimestamp() int64 {
+	return time.Now().UnixNano() / int64(time.Millisecond)
+}
