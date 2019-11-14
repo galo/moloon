55da33ac3700e188dc7a6c7cf53a82840a2c90b6
Galo Gimenez <galo@hp.com>
2019-11-14 09:45:13 -0700

    disco variables

--- disco/discovery.go
+++ disco/discovery.go
@@ -17,19 +17,24 @@ func NewDiscoveryService() DiscoveryService {
 	var d DiscoveryService
 
 	// determine the discovery service
-	switch ds := viper.GetString("discovery.config"); ds {
+	switch ds := viper.GetString("discovery_config"); ds {
+	// Kubernetes service discovery
 	case "kubernetes":
 		log.Println("Setting up Kubernetes discovery")
 		url := viper.GetString("url")
+
 		// Kubernetes in cluster
 		d = NewKubernetesDiscoveryService(url)
+
+	// File configured discovery
 	case "file":
 		log.Println("Setting up file based discovery")
 		//url := viper.GetString("agent_url")
 		// Kubernetes in cluster
-		a := viper.GetString("discovery.agents")
+		a := viper.GetString("discovery_agents")
 		log.Printf("Add agent %v for discovery \n", a)
 		d = NewConfigDiscovery("http://agent:3000/")
+
 	default:
 		log.Fatal("Discovery not supported ")
 	}
